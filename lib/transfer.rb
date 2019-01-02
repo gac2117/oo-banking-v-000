1aab336b6a004f0375d8295d34ebe9f1352a2f54
Grace Choi <oiograceoio@gmail.com>
2019-01-02 07:04:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,8 +24,10 @@ class Transfer
   end
 
   def reverse_transfer
-    sender.balance = sender.balance + amount
-    receiver.balance = receiver.balance - amount
-    self.status = "reversed"
+    if status == "complete"
+      sender.balance = sender.balance + amount
+      receiver.balance = receiver.balance - amount
+      self.status = "reversed"
+    end
   end
 end
