edeebbc55b4d416cd266358525cd9ad90a95a5ae
Grace Choi <oiograceoio@gmail.com>
2019-01-02 06:50:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
     if valid?
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
+      status = "complete"
     else
       "Transaction rejected. Please check your account balance."
     end
